5f6f86e09665e622f00d9015c9b1174b37afa818
matiaselm <matias@elm.fi>
2021-04-16 15:03:37 +0300

    new schemas

--- models/Note.js
+++ models/Note.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-    audio: { type: mongoose.Schema.types.ObjectID, ref: 'Audio' },  // From what audio the note is from
-    timestamp: Number,                                              // The bookmarks timestamp
-    data: String                                                    // Whatever the user has written up from that part
+    timestamp: Number,          // The bookmarks timestamp
+    data: String,               // Whatever the user has written up from that part
+    audioID: String,            // From what audio the note is from
+    userID: String,             // Id of the user
 });
 
 export default mongoose.model('Note', noteSchema);
--- models/User.js
+++ models/User.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    uid: String,                                                                 // Anonymous uid of the current user
-    progress: Number,                                                            // Progress of current audio
+    uid: String,        // Anonymous uid of the current user
+    name: String,       // Name of the user if they so want 
+    progress: Number,   // Progress of current audio
     audio: { type: mongoose.Schema.Types.ObjectID, ref: 'ConnectionType' },      // Current audio listening to
     notes: [{ type: mongoose.Types.ObjectID, ref: 'Note' }]                      // Collection of bookmarks the user has taken
 });
