2e7948060766e40bdcb2448320074cf5da8a9144
Matias Jalava <matias@elm.fi>
2021-05-03 17:31:29 +0300

    why isn't env working on jelastic

--- db/db.js
+++ db/db.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 const connectMongo = async () => {
+  console.log('db_url', process.env.DB_URL)
   try {
     const connection = await mongoose.connect(process.env.DB_URL,
         {
